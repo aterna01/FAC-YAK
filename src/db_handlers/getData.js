d6e4f6aae87c2588c25c8f07cd23ba45ff72c167
aterna01 <ssaniok90@mail.ru>
2018-12-13 10:14:06 +0000

    display data to console

---
+++ src/db_handlers/getData.js
@@ -0,0 +1,19 @@
+const dbConnection = require("../../database/db_connection");
+
+const getData = cb => {
+  dbConnection.query(
+    "SELECT name, title, date, time FROM users INNER JOIN talks ON users.id = talks.user_id",
+    (error, result) => {
+      if (error) {
+        console.log(error, "GET data error");
+        cb(error);
+      } else {
+        console.log(result.rows);
+        const talks = result.rows.slice();
+        cb(null, talks);
+      }
+    }
+  );
+};
+
+module.exports = getData;
