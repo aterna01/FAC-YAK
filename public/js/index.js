6517373df75dcb1fd556c2508240f8f9edc8f46a
susanX <susan.gooding@yahoo.co.uk>
2018-12-13 12:52:48 +0000

    form validation passwords

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,13 @@
 const populateSelect = function(data) {
   const ul = document.querySelector("#talk_list ul");
 
-  const list = data.map(item => 
-    // const dateFormat = item.date.slice(0,10);
-    `<li>
+  const list = data.map(
+    item =>
+      // const dateFormat = item.date.slice(0,10);
+      `<li>
       <span class="talk-name">${item.title}</span>
       <span class="person">${item.name}</span>
-      <span class="date">${item.date.slice(0,10)}</span>
+      <span class="date">${item.date.slice(0, 10)}</span>
       <span class="time">${item.time}</span>
     </li>`
   );
@@ -15,3 +16,20 @@ const populateSelect = function(data) {
 };
 
 requestFunction("GET", "/getTalks", populateSelect);
+
+///font end validation
+document
+  .querySelector(".send-signup")
+  .addEventListener("click", function(event) {
+    event.preventDefault();
+    const password = document.querySelector("#password");
+    const confirmPassword = document.querySelector("#confirm-password");
+    if (password.value !== confirmPassword.value) {
+      password.style.backgroundColor = "#bd3c59";
+      confirmPassword.style.backgroundColor = "#bd3c59";
+      console.log("wrong password");
+    } else {
+      password.style.backgroundColor = "#fff";
+      confirmPassword.style.backgroundColor = "#fff";
+    }
+  });
